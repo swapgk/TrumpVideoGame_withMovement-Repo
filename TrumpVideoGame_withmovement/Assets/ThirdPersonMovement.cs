cc2e5311373cabb9a741ecf2c2de5715a8817a62
Swapnil <swapnilgk.03@gmail.com>
2020-09-18 10:09:36 +0530

    update_1
    # This .gitignore file should be placed at the root of your Unity project directory
    #
    # Get latest from https://github.com/github/gitignore/blob/master/Unity.gitignore
    #
    [Ll]ibrary/
    [Tt]emp/
    [Oo]bj/
    [Bb]uild/
    [Bb]uilds/
    [Ll]ogs/
    [Uu]ser[Ss]ettings/
    
    # MemoryCaptures can get excessive in size.
    # They also could contain extremely sensitive data
    [Mm]emoryCaptures/
    
    # Asset meta data should only be ignored when the corresponding asset is also ignored
    !/[Aa]ssets/**/*.meta
    
    # Uncomment this line if you wish to ignore the asset store tools plugin
    # /[Aa]ssets/AssetStoreTools*
    
    # Autogenerated Jetbrains Rider plugin
    [Aa]ssets/Plugins/Editor/JetBrains*
    
    # Visual Studio cache directory
    .vs/
    
    # Gradle cache directory
    .gradle/
    
    # Autogenerated VS/MD/Consulo solution and project files
    ExportedObj/
    .consulo/
    *.csproj
    *.unityproj
    *.sln
    *.suo
    *.tmp
    *.user
    *.userprefs
    *.pidb
    *.booproj
    *.svd
    *.pdb
    *.mdb
    *.opendb
    *.VC.db
    
    # Unity3D generated meta files
    *.pidb.meta
    *.pdb.meta
    *.mdb.meta
    
    # Unity3D generated file on crash reports
    sysinfo.txt
    
    # Builds
    *.apk
    *.aab
    *.unitypackage
    
    # Crashlytics generated file
    crashlytics-build.properties
    
    # Packed Addressables
    [Aa]ssets/[Aa]ddressable[Aa]ssets[Dd]ata/*/*.bin*
    
    # Temporary auto-generated Android Assets
    [Aa]ssets/[Ss]treamingAssets/aa.meta
    [Aa]ssets/[Ss]treamingAssets/aa/*
    
    #customize files ignore
    TrumpVideoGame_withmovement/[Aa]ssets/SampleScenes/
    TrumpVideoGame_withmovement/Assets/Standard Assets/
    #Assets/OtherAssets/
    #Assets/Plugins/
    #Assets/Standard Assets/
    #Assets/AssetStoreTools/

---
+++ TrumpVideoGame_withmovement/Assets/ThirdPersonMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    private Animation anim;
+	private Rigidbody rb;
+	
+	public float turnSmoothTime= 0.1f;
+	private float turnSmoothVelocity;
+	
+	public Transform cam;  		 //to change the character in the direction the camera is facing
+
+
+	
+	// Use this for initialization
+	void Start (){
+		anim = GetComponent<Animation> ();
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		
+		
+		float x1 = Input.GetAxisRaw("Horizontal");
+		float y1 = Input.GetAxisRaw("Vertical");
+		
+		float x2 = CrossPlatformInputManager.GetAxis ("Horizontal");
+		float y2 = CrossPlatformInputManager.GetAxis ("Vertical");
+
+		float x=0;
+		float y=0;
+		//transform.position += new Vector3(0,0,y/100);
+		//transform.position += new Vector3(x/100,0,0);
+
+		//Vector3 movement = new Vector3(x, 0.0f, y).normalized;
+
+		//enter trumps speed here!!!
+		//rb.velocity = movement * 4f;
+		
+		if(x1+y1 !=0 ){
+			x=x1;
+			y=y1;
+		}
+		if(x2+y2 !=0 ){
+			x=x2;
+			y=y2;
+		}
+   
+		if (x != 0 || y != 0) {
+			float targetangle=Mathf.Atan2 (x, y) * Mathf.Rad2Deg + cam.eulerAngles.y; //(addition) 		 //to change the character in the direction the camera is facing
+
+			float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetangle,ref turnSmoothVelocity,turnSmoothTime);
+			transform.eulerAngles = new Vector3 (transform.eulerAngles.x,angle , transform.eulerAngles.z);
+			
+			Vector3 moveDirection= Quaternion.Euler(x,targetangle,y)*Vector3.forward;
+			rb.velocity = moveDirection.normalized * 4f;
+			anim.Play ("walk");
+		} else {
+			anim.Play ("idle");
+		}
+	}
+}
+
+
+
